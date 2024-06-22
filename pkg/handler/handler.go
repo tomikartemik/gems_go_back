ff96d40bcf0c4e958603b3804089789222e49a6c
tomikartemik <artemules@mail.ru>
2024-06-23 01:58:55 +0300

    url => *

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -34,7 +34,7 @@ func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "https://brawl-alpha.vercel.app", "ws://api.youngrusssia.ru", "ws://api.youngrusssia.ru"}, // Укажите домены, которым разрешен доступ
+		AllowOrigins:     []string{"*"}, // Укажите домены, которым разрешен доступ
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization", "X-Requested-With", "Content-Type", "Accept"},
 		ExposeHeaders:    []string{"Content-Length"},
