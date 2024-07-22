5a1c5e5ef5d61dea56d74226ce4f419fb0b77874
tomikartemik <artemules@mail.ru>
2024-07-22 03:21:59 +0300

    change statuses in .sh

--- deploy_gems.sh
+++ deploy_gems.sh
@@ -3,6 +3,7 @@
 SESSION_NAME="back"
 REPO_DIR="$HOME/gems_github/gems_go_back"
 GIT_REPO_URL="https://github.com/tomikartemik/gems_go_back"
+STATUS_URL="https://api.youngrusssia.ru/admin/change-status"
 
 echo "Starting deployment script"
 
@@ -10,6 +11,20 @@ echo "Starting deployment script"
 tmux_send() {
     tmux send-keys -t $SESSION_NAME "$1" C-m
 }
+
+# Функция для отправки запросов для изменения статуса
+change_status() {
+    local status=$1
+    curl -X POST "$STATUS_URL" \
+        -H "Content-Type: application/json" \
+        -d "{\"status\": $status}"
+}
+
+echo "Changing game status to false..."
+change_status false
+
+echo "Waiting for 30 seconds..."
+sleep 30
 
 echo "Killing existing tmux session (if any)"
 tmux kill-session -t $SESSION_NAME
@@ -25,5 +40,8 @@ echo "Starting new tmux session"
 tmux new-session -d -s $SESSION_NAME
 tmux_send "cd $REPO_DIR/cmd"
 tmux_send "go run main.go"
+
+echo "Changing game status to true..."
+change_status true
 
 echo "Deployment script finished"
