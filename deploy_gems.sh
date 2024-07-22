013324e14c9a7aca500cd02339e7043766fb46d5
tomikartemik <artemules@mail.ru>
2024-07-22 03:00:45 +0300

    change user to gems_github

--- deploy_gems.sh
+++ deploy_gems.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
 SESSION_NAME="back"
-REPO_DIR="$HOME/artem/gems_go_back"
+REPO_DIR="$HOME/gems_github/gems_go_back"
+GIT_REPO_URL="https://github.com/tomikartemik/gems_go_back"
 
+echo "Starting deployment script"
+
+# Функция для выполнения команд в tmux
 tmux_send() {
     tmux send-keys -t $SESSION_NAME "$1" C-m
 }
 
+echo "Killing existing tmux session (if any)"
 tmux kill-session -t $SESSION_NAME
+
+echo "Cloning/updating repository"
+if [ ! -d "$REPO_DIR" ]; then
+    git clone $GIT_REPO_URL $REPO_DIR
+fi
 
 cd $REPO_DIR && git pull origin main
 
-# Запуск нового экземпляра в новой tmux сессии
+echo "Starting new tmux session"
 tmux new-session -d -s $SESSION_NAME
 tmux_send "cd $REPO_DIR/cmd"
 tmux_send "go run main.go"
+
+echo "Deployment script finished"
