c8e71d5a16819ff1cda3831cd4bce1b1ac15eed0
tomikartemik <artemules@mail.ru>
2024-09-01 22:37:08 +0300

    photolinks in prices

--- pkg/model/prices.go
+++ pkg/model/prices.go
@@ -4,4 +4,5 @@ type Price struct {
 	ID       int     `json:"id" gorm:"primary_key;AUTO_INCREMENT"`
 	Position string  `json:"position"`
 	Price    float64 `json:"price"`
+	Photo    string  `json:"photo"`
 }
