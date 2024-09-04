0c67a38a2e5ee84ddc37fe5ce64940e2df18b1b7
tomikartemik <artemules@mail.ru>
2024-09-04 21:26:17 +0300

    SBP

--- pkg/service/replenishment.go
+++ pkg/service/replenishment.go
@@ -102,7 +102,7 @@ type CreateOrderResponse struct {
 }
 
 func createSignature(shopID int, amount int, currency string, email string, i int, ip string, nonce int, secretKey string) string {
-	message := fmt.Sprintf("%f|%s|%s|%d|%s|%d|%d", amount, currency, email, i, ip, nonce, shopID)
+	message := fmt.Sprintf("%d|%s|%s|%d|%s|%d|%d", amount, currency, email, i, ip, nonce, shopID)
 	fmt.Println(message)
 	h := hmac.New(sha256.New, []byte(secretKey))
 	h.Write([]byte(message))
