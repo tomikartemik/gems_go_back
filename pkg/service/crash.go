b6f47e6780d93f46b0674c0ad272a8d65d153d1d
tomikartemik <artemules@mail.ru>
2024-10-27 18:48:13 +0300

    test -> working crash

--- pkg/service/crash.go
+++ pkg/service/crash.go
@@ -180,8 +180,7 @@ func (s *CrashService) StartPreparingCrash() {
 
 func (s *CrashService) PreparingCrash() {
 	go s.GenerateFakeBetsCrash()
-	//for time_before_start := 1000.0; time_before_start >= 0; time_before_start-- {
-	for time_before_start := 300.0; time_before_start >= 0; time_before_start-- {
+	for time_before_start := 1000.0; time_before_start >= 0; time_before_start-- {
 		time.Sleep(10 * time.Millisecond)
 		clientsMutexCrash.Lock()
 		responseCrash.TimeBeforeStart = time_before_start / 100.0
@@ -208,11 +207,9 @@ func (s *CrashService) StartGameCrash() {
 
 func (s *CrashService) GameCrash() {
 	for responseCrash.Multiplier < winMultiplier {
-		//time.Sleep(10 * time.Millisecond)
-		time.Sleep(1000 * time.Millisecond)
+		time.Sleep(10 * time.Millisecond)
 		//responseCrash.Multiplier = responseCrash.Multiplier * 1.0004
-		//responseCrash.Multiplier = math.Round(responseCrash.Multiplier*10003) / 10000
-		responseCrash.Multiplier = math.Round(responseCrash.Multiplier*1000300) / 1000000
+		responseCrash.Multiplier = math.Round(responseCrash.Multiplier*10003) / 10000
 		if responseCrash.Length <= 100.0 {
 			responseCrash.Length += 0.4
 		}
