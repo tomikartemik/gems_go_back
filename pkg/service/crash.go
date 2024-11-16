6035ca5d885b0d95ed843fcdb1937ceb533d3d66
tomikartemik <artemules@mail.ru>
2024-11-16 03:51:20 +0300

    wm -> 1.3

--- pkg/service/crash.go
+++ pkg/service/crash.go
@@ -173,8 +173,8 @@ func (s *CrashService) StartPreparingCrash() {
 	responseCrash.Length = 0.0
 	responseCrash.Status = "Pending"
 	u = rand.Float64()
-	winMultiplier = math.Pow(1-u, -1/2.25)
-	//winMultiplier = 1.01
+	//winMultiplier = math.Pow(1-u, -1/2.25)
+	winMultiplier = 1.3
 	lastGame, err := s.repo.GetLastCrashRecord()
 	if err != nil {
 		log.Fatal(err)
